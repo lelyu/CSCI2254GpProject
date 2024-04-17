42dd97bda6636ae369397f6cbb9a70fbb5d6dfe1
Dave <wangebb@bc.edu>
2024-04-17 19:18:42 -0400

    Navbar update

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,42 +1,27 @@
 import React from "react";
+import "./NavBar.css";
 
 const NavBar = () => {
-  return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-link active" aria-current="page" href="#">
-              Home
-            </a>
-            <a class="nav-link" href="#">
-              Features
-            </a>
-            <a class="nav-link" href="#">
-              Pricing
-            </a>
-            <a class="nav-link disabled" aria-disabled="true">
-              Disabled
-            </a>
-          </div>
+    const handleHover = (event) => {
+        const element = event.target;
+        element.classList.add('bounce');
+        setTimeout(() => {
+            element.classList.remove('bounce');
+        }, 2000); // 2秒后移除bounce类
+    };
+
+    return (
+        <div className="container-fluid">
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <a id="len1" className="nav-link hoverable" href="#" onMouseEnter={handleHover}>Home</a>
+                    <a id="len2" className="nav-link hoverable" href="#" onMouseEnter={handleHover}>About</a>
+                    <a id="len3" className="nav-link hoverable" href="#" onMouseEnter={handleHover}>Portfolio</a>
+                    <a id="len4" className="nav-link hoverable" href="#" onMouseEnter={handleHover}>Contact</a>
+                </div>
+            </nav>
         </div>
-      </div>
-    </nav>
-  );
+    );
 };
 
 export default NavBar;
