b7f1be33033b82627586d1402e1229479aedace1
Dave <wangebb@bc.edu>
2024-04-17 19:35:11 -0400

    New Navbar

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./NavBar.css";
 
 const NavBar = () => {
-    const handleHover = (event) => {
-        const element = event.target;
-        element.classList.add('bounce');
-        setTimeout(() => {
-            element.classList.remove('bounce');
-        }, 2000); // 2秒后移除bounce类
-    };
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const str = '#len'; //increment by 1 up to 1-nelemnts
+            let i = 1;
+            const stop = 4; //num elements
+            if (i > stop) {
+                clearInterval(interval);
+            }
+            const element = document.getElementById('len' + (i++));
+            element.addEventListener('mouseenter', () => {
+                element.classList.add('bounce');
+            });
+            element.addEventListener('mouseleave', () => {
+                element.classList.remove('bounce');
+            });
+        }, 500);
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className="container-fluid">
             <nav className="navbar navbar-inverse">
                 <div className="container-fluid">
-                    <a id="len1" className="nav-link hoverable" href="#" onMouseEnter={handleHover}>Home</a>
-                    <a id="len2" className="nav-link hoverable" href="#" onMouseEnter={handleHover}>About</a>
-                    <a id="len3" className="nav-link hoverable" href="#" onMouseEnter={handleHover}>Portfolio</a>
-                    <a id="len4" className="nav-link hoverable" href="#" onMouseEnter={handleHover}>Contact</a>
+                    <a id="len1" className="nav-link hoverable" href="#">Home</a>
+                    <a id="len2" className="nav-link hoverable" href="#">About</a>
+                    <a id="len3" className="nav-link hoverable" href="#">Portfolio</a>
+                    <a id="len4" className="nav-link hoverable" href="#">Contact</a>
                 </div>
             </nav>
         </div>
@@ -25,3 +36,5 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+
