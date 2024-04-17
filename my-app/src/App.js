5e2d075a13732b03fae29a6273201b52104c872c
Dave <wangebb@bc.edu>
2024-04-17 16:25:13 -0400

    new version

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,24 @@
-import logo from "./logo.svg";
+// import cs
 import "./App.css";
+// import basic things
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+// import components below
+
+
+
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+
+	
+		<Router>
+			<Routes>
+				{/* add the routes here */}
+			</Routes>
+		</Router>
+
+
+		
 	);
 }
 
