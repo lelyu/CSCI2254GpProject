076cafe3e015e1011a11ba08cfcf584a141939fb
Lok Leoi <lyulelok@gmail.com>
2024-04-18 19:11:12 -0400

    added route for login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import components below
 import Homepage from "./components/Homepage";
-
+import Login from "./components/Login";
 function App() {
 	return (
 		<Router>
 			<Routes>
 				{/* add the routes here */}
 				<Route exact path="/" element={<Homepage />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</Router>
 	);
--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { auth } from "../firebase";
-
+import firebase from "firebase";
+import { auth } from "../firebase.js";
 const Login = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
