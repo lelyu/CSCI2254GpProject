9777f6cba043c956cf36d3d38a6625ae693a8208
Dave <wangebb@bc.edu>
2024-04-19 16:13:52 -0400

    new

--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -4,7 +4,7 @@ import React from "react";
 const Intro = () => {
   return (
     <div className="intro-container animate__animated animate__fadeIn animate__slower">
-      <h2>Welcome to Our Website!</h2>
+      <h2>WELCOME TO STEP VENTUE</h2>
       <p>
         We are dedicated to managing weight and reducing carbon dioxide emissions to create a healthier and more sustainable future.
       </p>
