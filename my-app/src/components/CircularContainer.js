59d43955fb9e5f2fb3d176e08d2a32ad40751a8d
Dave <wangebb@bc.edu>
2024-04-19 16:01:53 -0400

    new update

--- my-app/src/components/CircularContainer.js
+++ my-app/src/components/CircularContainer.js
@@ -2,18 +2,19 @@ import React from 'react';
 import '../css/CircularContainer.css'; // 引入样式文件
 
 const CircularContainer = ({ percentageSaved }) => {
-
+    const isGoalAchieved = percentageSaved === 100; // 判断是否达到目标
+    const displayText = percentageSaved > 100 ? 'You win' : `${percentageSaved.toFixed(1)}%`; // 根据条件设置显示文本
 
     return (
         <div className="circular-container">
-
             <div
                 className="circular-level"
-                style={{ transform: `scaleY(${percentageSaved / 100})` }}
+                style={{ transform: `scaleY(${Math.min(percentageSaved, 100) / 100})` }} // 限制最大值为 100%
             ></div>
-
-<p className="percentage-text">{percentageSaved.toFixed(1)}%</p> 
-
+            {isGoalAchieved && (
+                <p className="goal-message">You achieved the goal</p>
+            )}
+            <p className="percentage-text">{displayText}</p> {/* 显示百分比或文本 */}
         </div>
     );
 };
