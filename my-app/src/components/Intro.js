fa51bb0a835d87a9045699b059c9e426e5e054b9
Dave <wangebb@bc.edu>
2024-04-20 16:42:56 -0400

    n

--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Intro = () => {
   return (
-    <div className="intro-container animate__animated animate__fadeIn animate__slower">
+    <div>
       <h2>WELCOME TO STEP VENTUE</h2>
       <p>
       Welcome to STEP VENTURE, where we merge weight management with environmental stewardship. Join us as we tackle the pressing challenges of obesity and climate change through innovative, sustainable lifestyle choices. Together, we can transform our lives and our planet, one step at a time.
