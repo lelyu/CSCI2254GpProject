153b31c50bb6bdcd5450561323b3b434454bc825
Dave <wangebb@bc.edu>
2024-04-20 16:32:36 -0400

    new

--- my-app/src/components/Homepage.js
+++ my-app/src/components/Homepage.js
@@ -1,5 +1,6 @@
 // Homepage.js
 import React, { useState } from "react";
+import WOW from "wowjs";
 import NavBar from "./NavBar";
 import Intro from "./Intro";
 import Tree from "./Tree";
@@ -9,97 +10,110 @@ import GoogleMap from "./GoogleMap";
 import StepCounter from "./StepCounter";
 import ZipCodeInput from "./ZipCodeInput";
 import { useEffect } from "react";
+
+import "../css/Homepage.css";
 // import { getAuth, onAuthStateChanged } from "firebase/auth";
-// import { app } from "../firebase";
+import { app } from "../firebase";
 // import { useNavigate } from "react-router-dom";
 function Homepage() {
-	// const navigate = useNavigate();
-	// const [user, setUser] = useState(false);
+  // const navigate = useNavigate();
+  // const [user, setUser] = useState(false);
 
-	// useEffect(() => {
-	// 	const unsubscribe = onAuthStateChanged(getAuth(app), (user) => {
-	// 		console.log(user);
-	// 		if (!user) {
-	// 			navigate("/login");
-	// 		}
-	// 		setUser(!!user);
-	// // 	});
+  // useEffect(() => {
+  // 	const unsubscribe = onAuthStateChanged(getAuth(app), (user) => {
+  // 		console.log(user);
+  // 		if (!user) {
+  // 			navigate("/login");
+  // 		}
+  // 		setUser(!!user);
+  // // 	});
 
-	// 	return () => {
-	// 		unsubscribe();
-	// 	};
-	// }, [user, setUser, navigate]);
+  // 	return () => {
+  // 		unsubscribe();
+  // 	};
+  // }, [user, setUser, navigate]);
 
-	// function handleClick() {
-	// 	const auth = getAuth(app);
-	// 	auth.signOut();
-	// }
-	// console.log(user);
+  // function handleClick() {
+  // 	const auth = getAuth(app);
+  // 	auth.signOut();
+  // }
+  // console.log(user);
 
-	const [userZipCode, setUserZipCode] = useState(""); // Store user input ZIP code
-	// Handle user input ZIP code
-	const handleZipCodeSubmit = (zipCode) => {
-		setUserZipCode(zipCode); // Update user input ZIP code
-	};
+  const [userZipCode, setUserZipCode] = useState(""); // Store user input ZIP code
+  // Handle user input ZIP code
+  const handleZipCodeSubmit = (zipCode) => {
+    setUserZipCode(zipCode); // Update user input ZIP code
+  };
 
-	// Input steps, pass to the below StepCounter function
-	const handleStepChange = (newSteps) => {
-		console.log("New steps:", newSteps);
-	};
+  // Input steps, pass to the below StepCounter function
+  const handleStepChange = (newSteps) => {
+    console.log("New steps:", newSteps);
+  };
 
-	return (
-		<div className="container">
-			<NavBar />
+  useEffect(() => {
+    new WOW.WOW({
+      boxClass: "wow", // Default CSS class for Wow.js animations
+      animateClass: "animated", // Default CSS class for animation library (animate.css)
+      offset: 0, // Change this value to adjust the viewport offset for triggering animations
+      mobile: false, // Enable animations on mobile devices
+      live: true, // Apply changes to the DOM after adding new elements
+    }).init();
+  }, []); // Empty dependency array ensures the effect runs only once on component mount
 
-			<div className="row">
-				{/* Intro */}
-				<div className="col-md-6 animate__animated animate__fadeInLeft">
-					<Intro />
-				</div>
-				{/* Tree */}
-				<div className="col-md-6 animate__animated animate__fadeInRight">
-					<Tree />
-				</div>
-			</div>
 
-			<div className="row">
-				{/* Mission */}
-				<div className="col-md-6 animate__animated animate__fadeInLeft">
-					<Mission />
-				</div>
-				{/* Earth */}
-				<div className="col-md-6 animate__animated animate__fadeInRight">
-					<Earth />
-				</div>
-			</div>
 
-			<div className="row">
-				<div className="col-md-6">
-					<div className="col animate__animated animate__fadeInLeft animate__slower">
-						<StepCounter onStepChange={handleStepChange} />
-					</div>
-				</div>
-				<div className="col-md-6">
-					<div className="row">
-						<div className="col animate__animated animate__fadeInLeft animate__slower">
-							<ZipCodeInput
-								onZipCodeSubmit={handleZipCodeSubmit}
-							/>
-						</div>
-					</div>
-					{/* GoogleMap */}
-					<div className="row">
-						<div className="animate__animated animate__fadeInRight animate__slower">
-							<GoogleMap
-								apiKey="AIzaSyDqvqXMzvIFpaIkCMPNh-TmOnMzZymUUAg"
-								zipCode={userZipCode}
-							/>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="container">
+  <NavBar />
+
+  <div className="row">
+    {/* Intro */}
+    <div className="col-md-6 wow animate__animated animate__fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+      <Intro />
+    </div>
+    {/* Tree */}
+    <div className="col-md-6 wow animate__animated animate__fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+      <Tree />
+    </div>
+  </div>
+
+  <div className="row">
+    <div className="col-md-6 wow animate__animated animate__fadeInLeft" data-wow-duration="1s" data-wow-delay="0.5s">
+      <Earth />
+    </div>
+    {/* Mission */}
+    <div className="col-md-6 wow animate__animated animate__fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+      <Mission />
+    </div>
+  </div>
+
+  <div className="row">
+    <div className="col-md-6">
+      <div className="col wow animate__animated animate__fadeInLeft animate__slower" data-wow-duration="2s" data-wow-delay="1s">
+        <StepCounter onStepChange={handleStepChange} />
+      </div>
+    </div>
+    <div className="col-md-6">
+      <div className="row">
+        <div className="col wow animate__animated animate__fadeInLeft animate__slower" data-wow-duration="2s" data-wow-delay="1s">
+          <ZipCodeInput onZipCodeSubmit={handleZipCodeSubmit} />
+        </div>
+      </div>
+      {/* GoogleMap */}
+      <div className="row">
+        <div className="col wow animate__animated animate__fadeInRight animate__slower" data-wow-duration="2s" data-wow-delay="1s">
+          <GoogleMap
+            apiKey="AIzaSyDqvqXMzvIFpaIkCMPNh-TmOnMzZymUUAg"
+            zipCode={userZipCode}
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  );
 }
 
 export default Homepage;
