17e8a39ad71a4fd715f347ec43d2aa70e0ad4a33
Dave <wangebb@bc.edu>
2024-04-20 17:23:42 -0400

    new

--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -5,7 +5,7 @@ import "../css/Intro.css";
 const Intro = () => {
   return (
     <div>
-      <h2>Who Are We</h2>
+      <h1>Who Are We</h1>
       <p>
       Welcome to Step Venture! We are a dedicated team of developers passionate about creating solutions that integrate health improvement and environmental sustainability. Founded by Le Lyu, Yuqi Wang, and Michael Lebreck under the mentorship of Professor Mira Yun, our website serves as a platform to inspire and support individuals in leading healthier and more environmentally friendly lives. Our multidisciplinary team combines expertise in web development, health sciences, and environmental studies to bring you innovative tools and insightful content. 
       </p>
--- my-app/src/components/Mission.js
+++ my-app/src/components/Mission.js
@@ -6,7 +6,7 @@ import React from 'react';
 const Mission = () => {
   return (
     <div className="Mission-container animate__animated animate__fadeIn animate__slower">
-      <h2>Our Mission</h2>
+      <h1>Our Mission</h1>
       <p>
       Step Venture is dedicated to empowering individuals to enhance their health and lessen their environmental impact, tackling both obesity and climate change. We promote physical activities like walking and cycling as both beneficial exercise and eco-friendly transport. Our platform champions environmental responsibility by advocating for energy-efficient practices and raising awareness about the environmental impacts of everyday actions. We're committed to building a vibrant community through engaging outreach and diverse collaborations that boost health and sustainability. By leveraging innovative tools and expert partnerships, we aim to inspire a movement where each step toward personal health also advances a healthier planet.
       </p>
