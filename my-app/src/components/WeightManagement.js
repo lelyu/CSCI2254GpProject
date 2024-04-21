25a3144686c1915490f9f7b4579cf713e7a1b75e
Dave <wangebb@bc.edu>
2024-04-21 11:50:32 -0400

    chart update

--- my-app/src/components/WeightManagement.js
+++ my-app/src/components/WeightManagement.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import BarChart from "./BarChart";
 import LineChart from "./LineChart";
+import "../css/WeightManagement.css";
 
 function WeightManagement() {
-  const [weight, setWeight] = useState(''); // State for storing user's input weight
+  const [weight, setWeight] = useState(""); // State for storing user's input weight
   const [weightData, setWeightData] = useState([]); // State for storing weight data as an array
-
 
   // Effect hook to load weight data from local storage
   useEffect(() => {
-    const storedWeightData = localStorage.getItem('weightData');
+    const storedWeightData = localStorage.getItem("weightData");
     if (storedWeightData) {
       setWeightData(JSON.parse(storedWeightData));
     }
@@ -18,21 +18,30 @@ function WeightManagement() {
 
   // Function to handle weight submission and update local storage
   const handleWeightSubmit = () => {
-    if (weight !== '') {
-      const newWeightData = [...weightData, { date: new Date().toLocaleDateString(), weight: parseFloat(weight) }];
+    if (weight !== "") {
+      const newWeightData = [
+        ...weightData,
+        { date: new Date().toLocaleDateString(), weight: parseFloat(weight) },
+      ];
       setWeightData(newWeightData);
-      localStorage.setItem('weightData', JSON.stringify(newWeightData)); // Store updated weight data in local storage
-      setWeight(''); // Reset the weight input
+      localStorage.setItem("weightData", JSON.stringify(newWeightData)); // Store updated weight data in local storage
+      setWeight(""); // Reset the weight input
     }
   };
 
   return (
-    <div className="container">
+    <div className="contianer">
       <NavBar />
-      <div>
-        <BarChart data={weightData} />
+      <div className="row">
+        <div className="col-md-6">
+          <BarChart data={weightData} />
+        </div>
+        <div className="col-md-6">
+          <LineChart data={weightData} />
+        </div>
+      </div>
 
-        <LineChart data={weightData} />
+      <div className="row">
         <input
           type="number"
           value={weight}
