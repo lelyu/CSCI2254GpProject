a24b0444c587fc83221ea02c162e21e51d427bab
Dave <wangebb@bc.edu>
2024-04-26 13:07:55 -0400

    new

--- my-app/src/components/StepCounter.js
+++ my-app/src/components/StepCounter.js
@@ -3,13 +3,39 @@ import People from "../images/people.gif";
 import React, { useState } from "react";
 import CircularContainer from "./CircularContainer"; // 导入 CircularContainer 组件
 
+// Evidence
+import { storage } from "../firebase.js"; // 引入storage
+import { ref, uploadBytes } from "firebase/storage";
+
 const StepCounter = ({ onStepChange }) => {
   const [steps, setSteps] = useState(""); // 用于跟踪用户输入的步数
   const [distance, setDistance] = useState(""); // 用于跟踪计算出的步行距离
   const [carbonSaved, setCarbonSaved] = useState(0); // 用于跟踪计算出的二氧化碳节省量
-
-
+  const [file, setFile] = useState(null); // evidence
   // 处理输入框变化
+
+  // Evidence Upload
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = () => {
+    if (!file) {
+      alert("Please select a file first!");
+      return;
+    }
+
+    const fileRef = ref(storage, `evidence/${file.name}`);
+    uploadBytes(fileRef, file)
+      .then((snapshot) => {
+        alert("File uploaded successfully");
+      })
+      .catch((error) => {
+        console.error("Error uploading file:", error);
+        alert("Error uploading file");
+      });
+  };
+
   const handleChange = (e) => {
     setSteps(e.target.value); // 更新步数
   };
@@ -24,7 +50,6 @@ const StepCounter = ({ onStepChange }) => {
     // 计算二氧化碳节省量（假设 1 公里 = 0.27 公斤 CO2 节省）
     const carbonSavedValue = (walkingDistance / 1000) * 0.27; // 将米转换为公里并计算 CO2 节省量
     setCarbonSaved(carbonSavedValue.toFixed(2)); // 设置 CO2 节省量并保留两位小数
-
   };
 
   // 计算二氧化碳减少的百分比
@@ -44,6 +69,17 @@ const StepCounter = ({ onStepChange }) => {
         <button type="submit" className="btn btn-outline-success">
           Submit
         </button>
+
+        {/* Evidence  */}
+
+        <input type="file" onChange={handleFileChange} />
+        <button
+          type="button"
+          onClick={handleFileUpload}
+          className="btn btn-outline-secondary"
+        >
+          Upload Evidence
+        </button>
       </form>
 
       {/* Always display the walking distance label, but only show the value after submit */}
@@ -52,15 +88,12 @@ const StepCounter = ({ onStepChange }) => {
         <div className="col-md-6 ">
           <div className="walking-distance">
             <p>You have walked {distance ? `${distance} meters` : ""}</p>
-      
-              <img
-                src={People}
-                alt="Description of Image"
-                className="side-image wow fadeInLeft"
-              />
- 
-              
 
+            <img
+              src={People}
+              alt="Description of Image"
+              className="side-image wow fadeInLeft"
+            />
           </div>
         </div>
 
